4ecf9c802f5f46bf2a7636fd1e2d0ae81fe89859
Piotr Walen <walen.piotr@gmail.com>
2017-12-05 15:45:02 +0100

    Add exisiting file Move load_images_from_folder from letter_slicer.ipynb Move show_images from letter_slicer.ipynb Move BGR_to_RBG from letter_slicer.ipynb

---
+++ utils.py
@@ -0,0 +1,32 @@
+import cv2
+import numpy as np
+import os
+from matplotlib import pyplot as plt
+plt.rcParams.update({'font.size': 4})
+
+
+def load_images_from_folder(folder):
+    images = []
+    for filename in os.listdir(folder):
+        img = cv2.imread(os.path.join(folder,filename))
+        if img is not None:
+            images.append(img)
+    return images
+
+def show_images(images,figsize,dpi,grid):
+
+    number_of_subplots = len(images)
+    plt.figure(figsize = (figsize,figsize), dpi = dpi)
+    
+    for v in range(number_of_subplots):
+        ax1 = plt.subplot(grid[0],grid[1],v+1)
+        plt.title('img #' + str(v))
+        ax1.imshow(images[v],  cmap='gray')
+    plt.show()
+    
+def BGR_to_RBG(images):
+    RBG_images = []
+    for image in images:
+        RBG_images.append(image[...,::-1])
+    return RBG_images
+    
