782672f19e565d290adb53fda031806eb9c61038
WalenPiotr <walen.piotr@gmail.com>
2018-01-25 23:42:07 +0100

    Add calculateAngle method Add tests for calculateAngle method Modify sortPointsClockwise method Add test for sortPointsClockwise method

--- app/src/main/java/com/example/piotr/scrabble_detector/ImageProcessing.java
+++ app/src/main/java/com/example/piotr/scrabble_detector/ImageProcessing.java
@@ -13,13 +13,15 @@ import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 class ImageProcessing {
 
 
-    static Bitmap warp(Bitmap bitmap){
+    static Bitmap warp(Bitmap bitmap) {
         Mat sourceMat = createMat(bitmap);
         Mat outputMat = preprocessMat(sourceMat);
         List<Point> corners = ImageProcessing.findCorners(outputMat);
@@ -28,28 +30,27 @@ class ImageProcessing {
     }
 
 
-
-    static List<Bitmap> slice (Bitmap bitmap, int output_size){
+    static List<Bitmap> slice(Bitmap bitmap, int output_size) {
         Mat outputMat = createMat(bitmap);
-        Size size = new Size(output_size,output_size);
+        Size size = new Size(output_size, output_size);
         List<Mat> slices = ImageProcessing.sliceMat(outputMat, size);
         List<Bitmap> bitmaps = new ArrayList<>();
-        for (int i = 0; i<slices.size(); i++) {
+        for (int i = 0; i < slices.size(); i++) {
             bitmaps.add(createBitmap(slices.get(i)));
         }
 
         return bitmaps;
     }
 
-    static Mat createMat(Bitmap bitmap){
+    static Mat createMat(Bitmap bitmap) {
         Mat imageMat = new Mat();
         Utils.bitmapToMat(bitmap, imageMat);
-        Imgproc.cvtColor(imageMat,imageMat,Imgproc.COLOR_BGRA2BGR);
+        Imgproc.cvtColor(imageMat, imageMat, Imgproc.COLOR_BGRA2BGR);
 
         return imageMat;
     }
 
-    static Bitmap createBitmap(Mat imageMat){
+    static Bitmap createBitmap(Mat imageMat) {
         Bitmap outputBitmap = Bitmap.createBitmap(imageMat.cols(), imageMat.rows(),
                 Bitmap.Config.RGB_565);
         Utils.matToBitmap(imageMat, outputBitmap);
@@ -71,7 +72,7 @@ class ImageProcessing {
         return imageMat;
     }
 
-    static List<Point> findCorners(Mat preprocessedMat){
+    static List<Point> findCorners(Mat preprocessedMat) {
         List<Point> points = Collections.emptyList();
         List<MatOfPoint> contours = new ArrayList<>();
         Imgproc.findContours(preprocessedMat, contours, new Mat(), Imgproc.RETR_LIST,
@@ -122,8 +123,8 @@ class ImageProcessing {
     }
 
     static Point findCenterPoint(List<Point> points) throws IllegalArgumentException {
-        if(points == null) {
-            throw new IllegalArgumentException("method argument cannot be null");
+        if (points == null) {
+            throw new IllegalArgumentException("Method argument cannot be null");
         }
         int sumX = 0;
         int sumY = 0;
@@ -135,27 +136,32 @@ class ImageProcessing {
         return new Point(sumX / n, sumY / n);
     }
 
-    static List<Point> sortPointsClockwise(List<Point> points) {
-        Point centerPoint = findCenterPoint(points);
-        double x_center = centerPoint.x;
-        double y_center = centerPoint.y;
-
-        List<Point> sortedPoints = new ArrayList<>();
-        if (points.size() == 4) {
-            for (Point point : points) {
-                if (point.x <= x_center && point.y <= y_center) {
-                    sortedPoints.add(0, point);
-                } else if (point.x <= x_center && point.y > y_center) {
-                    sortedPoints.add(1, point);
-                } else if (point.x > x_center && point.y > y_center) {
-                    sortedPoints.add(2, point);
-                } else if (point.x > x_center && point.y <= y_center) {
-                    sortedPoints.add(3, point);
-                }
-            }
+    static List<Point> sortPointsClockwise(List<Point> points) throws IllegalArgumentException {
+        if (points == null) {
+            throw new IllegalArgumentException("Method argument cannot be null");
+        }
+        if (points.size() == 1) {
+            return points;
         }
 
-        return sortedPoints;
+        final Point center = findCenterPoint(points);
+
+        Collections.sort(points,new Comparator<Point>() {
+            @Override
+            public int compare(Point p1, Point p2) {
+                return (int) (-calculateAngle(center,p1) + calculateAngle(center,p2));
+            }
+        });
+
+        return points;
+    }
+
+    static double calculateAngle(Point p1, Point p2) throws IllegalArgumentException{
+        if (p1 == null || p2 == null) {
+            throw new IllegalArgumentException("Method argument cannot be null");
+        }
+        double angle = Math.toDegrees(Math.atan2(p2.y - p1.y, p2.x - p1.x));
+        return angle >= 0 ? angle : angle + 360.0;
     }
 
     static Mat warpMat(List<Point> sortedPoints, Mat sourceImageMat) {
@@ -185,17 +191,17 @@ class ImageProcessing {
         ArrayList<Mat> slices = new ArrayList<>();
         int width = image.width();
         int height = image.height();
-        int slice_width = width/15;
-        int slice_height = height/15;
+        int slice_width = width / 15;
+        int slice_height = height / 15;
 
-        Log.i("OpenCV","slice_width= "+Integer.toString(slice_width) + " slice_height= "+ Integer.toString(slice_height));
+        Log.i("OpenCV", "slice_width= " + Integer.toString(slice_width) + " slice_height= " + Integer.toString(slice_height));
         if (width > 0 && height > 0) {
-            for (int i = 0; i<15; i++) {
-                for (int j = 0; j<15; j++) {
-                    int x = i*slice_width;
-                    int y = j*slice_height;
+            for (int i = 0; i < 15; i++) {
+                for (int j = 0; j < 15; j++) {
+                    int x = i * slice_width;
+                    int y = j * slice_height;
                     //Log.i("OpenCV", "x = " + Integer.toString(x) + " y = "+Integer.toString(y));
-                    Mat slice = image.submat(x,x+slice_width,y,y+slice_width);
+                    Mat slice = image.submat(x, x + slice_width, y, y + slice_width);
 
                     //Log.i("OpenCV", "slice "+slice.toString());
                     Mat outSlice = new Mat();
--- app/src/test/java/com/example/piotr/scrabble_detector/ImageProcessingTest.java
+++ app/src/test/java/com/example/piotr/scrabble_detector/ImageProcessingTest.java
@@ -3,6 +3,7 @@ package com.example.piotr.scrabble_detector;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
 import org.opencv.core.Point;
 
 import static org.hamcrest.core.IsEqual.equalTo;
@@ -12,21 +13,26 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+
+
+@RunWith(JUnitParamsRunner.class)
 public class ImageProcessingTest {
     @Test
-    public void findCenter_shouldOutputTheSamePoint_whenListWithOnePointIsPassed(){
-        Point expected = new Point(-20,10);
+    public void findCenter_ListWithOnePointIsPassed() {
+        Point expected = new Point(-20, 10);
         Point actual = ImageProcessing.findCenterPoint(Collections.singletonList(expected));
         assertThat(actual, equalTo(expected));
     }
 
     @Test
-    public void findCenter_shouldOutputCenterPointOfLine_whenListWithTwoPointsIsPassed(){
+    public void findCenter_ListWithTwoPointsIsPassed() {
         List<Point> points = Arrays.asList(
-                new Point(-10,0),
+                new Point(-10, 0),
                 new Point(-2, 10)
         );
-        Point expected = new Point(-6,5);
+        Point expected = new Point(-6, 5);
         Point actual = ImageProcessing.findCenterPoint(points);
         assertThat(actual, equalTo(expected));
     }
@@ -35,13 +41,66 @@ public class ImageProcessingTest {
     public final ExpectedException exception = ExpectedException.none();
 
     @Test
-    public void findCenter_shouldThrowIllegalArgumentException_whenNullIsPassed() {
+    public void findCenter_NullIsPassed() {
         exception.expect(IllegalArgumentException.class);
-        exception.expectMessage("method argument cannot be null");
+        exception.expectMessage("Method argument cannot be null");
         ImageProcessing.findCenterPoint(null);
     }
 
+    @Test
+    public void sortPointsClockwise_NullIsPassed() {
+        exception.expect(IllegalArgumentException.class);
+        exception.expectMessage("Method argument cannot be null");
+        ImageProcessing.sortPointsClockwise(null);
+    }
 
+    @Test
+    public void sortPointClockwise_OnePointIsPassed() {
+        List<Point> points = Collections.singletonList(new Point(-1, 10));
+        assertThat(ImageProcessing.sortPointsClockwise(points), equalTo(points));
+    }
+
+
+    @Test
+    public void sortPointClockwise_ValidPointListIsPassed() {
+        List<Point> input = Arrays.asList(
+                new Point(-1, -1),
+                new Point(1, -1),
+                new Point(0, 2)
+        );
+
+        List<Point> expected = Arrays.asList(
+                new Point(0, 2),
+                new Point(-1, -1),
+                new Point(1, -1)
+        );
+
+        assertThat(ImageProcessing.sortPointsClockwise(input), equalTo(expected));
+
+    }
+
+
+    @Test
+    public void calculateAngle_whenNullIsPassed() {
+        exception.expect(IllegalArgumentException.class);
+        exception.expectMessage("Method argument cannot be null");
+        ImageProcessing.calculateAngle(null, new Point(2, 1));
+    }
+
+    private static Object[] calculateAngleTestData() {
+        return new Object[] {
+                new Object[] {new Point(0,0), new Point(1,1), 45.0},
+                new Object[] {new Point(0,0), new Point(-1,-1), 225.0},
+                new Object[] {new Point(0,0), new Point(0,0), 0.0},
+                new Object[] {new Point(0,0), new Point(1,-1), 315.0},
+        };
+    }
+
+    @Test
+    @Parameters(method = "calculateAngleTestData")
+    public void calculateAngle_whenValidPointsArePassed(Point p1, Point p2, double expected) {
+        assertThat(ImageProcessing.calculateAngle(p1, p2), equalTo(expected));
+    }
 
 
 }
